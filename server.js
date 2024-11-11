457eba00e0911893905d6d63d9bdd8cbcb337252
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2024-11-11 18:01:48 +0330

    testing

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from "express"
+import express from "express";
 
-const server = express()
+const server = express();
 
-const SERVER_PORT = process.env["SERVER_PORT"] || 3000
+server.get("/", (_, res) => res.send("Sup"));
+
+const SERVER_PORT = process.env["SERVER_PORT"] || 3000;
 server.listen(SERVER_PORT, () =>
-  console.log(`Server is running on ${SERVER_PORT}`),
-)
+  console.log(`Server is running on ${SERVER_PORT}`)
+);
